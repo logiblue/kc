d9c5460e80b87e1da0c33c50d807eab13a4c12b1
logiblue <karanikolasms@gmail.com>
2021-10-29 18:52:22 +0300

    logo

--- header.php
+++ header.php
@@ -40,24 +40,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 			<div class="container">
 		<?php endif; ?>
 
-					<!-- Your site title as branding in the menu -->
-					<?php if ( ! has_custom_logo() ) { ?>
+		<img src="<?php echo get_site_url(); ?>/wp-content/uploads/2021/10/logo.svg" alt="">
 
-						<?php if ( is_front_page() && is_home() ) : ?>
-
-							<!-- <h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1> -->
-							<img src="<?php echo get_site_url(); ?>/wp-content/uploads/2021/10/logo.svg" alt="">
-						<?php else : ?>
-
-							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
-
-						<?php endif; ?>
-
-						<?php
-					} else {
-						the_custom_logo();
-					}
-					?>
 					<!-- end custom logo -->
 
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
